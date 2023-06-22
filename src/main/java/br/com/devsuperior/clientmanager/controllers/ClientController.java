2ee6c6a016edf4e8d5de04bf0ec1be520957fcfb
alison <adevpr11@gmail.com>
2023-06-22 19:04:06 -0300

    Adiciona atualização do recurso

--- src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
+++ src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -24,5 +24,22 @@ public class ClientController {
   @GetMapping(value = "/{id}")
   public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
     return ResponseEntity.ok(service.findById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+    dto = service.insert(dto);
+    URI uri =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(dto.getId())
+            .toUri();
+    return ResponseEntity.created(uri).body(dto);
+  }
+
+  @PutMapping(value = "/{id}")
+  public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+    dto = service.update(id, dto);
+    return ResponseEntity.ok(dto);
   }
 }
