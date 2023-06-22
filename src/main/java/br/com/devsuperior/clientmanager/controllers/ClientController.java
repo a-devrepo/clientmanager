d74b596dbd5a339f169511c9782d87aab065c5d4
alison <adevpr11@gmail.com>
2023-06-22 16:43:06 -0300

    Adiciona ClientController

--- src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
+++ src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
@@ -1,4 +1,4 @@
-package br.com.devsuperior.clientmanager;
+package br.com.devsuperior.clientmanager.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
-    @GetMapping
-    public String test(){
-        return "Testing API";
-    }
+  @GetMapping
+  public String testApi() {
+    return "Testing API";
+  }
 }
