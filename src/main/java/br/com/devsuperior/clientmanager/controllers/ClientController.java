0ebe805528ec786254e39cd23d74f32811d2c005
alison <adevpr11@gmail.com>
2023-06-22 18:30:36 -0300

    Adiciona busca paginada

--- src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
+++ src/main/java/br/com/devsuperior/clientmanager/controllers/ClientController.java
@@ -1,5 +1,11 @@
 package br.com.devsuperior.clientmanager.controllers;
 
+import br.com.devsuperior.clientmanager.dto.ClientDTO;
+import br.com.devsuperior.clientmanager.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
+  @Autowired private ClientService service;
+
   @GetMapping
-  public String testApi() {
-    return "Testing API";
+  public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+    return ResponseEntity.ok(service.findAll(pageable));
   }
 }
--- src/main/java/br/com/devsuperior/clientmanager/services/ClientService.java
+++ src/main/java/br/com/devsuperior/clientmanager/services/ClientService.java
@@ -1,8 +1,19 @@
 package br.com.devsuperior.clientmanager.services;
 
+import br.com.devsuperior.clientmanager.dto.ClientDTO;
+import br.com.devsuperior.clientmanager.entities.Client;
+import br.com.devsuperior.clientmanager.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
-    
+  @Autowired private ClientRepository repository;
+
+  public Page<ClientDTO> findAll(Pageable pageable) {
+    Page<Client> result = repository.findAll(pageable);
+    return result.map(x -> new ClientDTO(x));
+  }
 }
